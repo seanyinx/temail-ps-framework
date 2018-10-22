d0fa0a4e882e1a87fcf7b15dca598de873383c8e
姚华成 <yaohuacheng@syswin.com>
2018-10-22 19:42:33 +0800

    增加AutoDecryptPsServer的javadoc 将decrypt相关的类变成包可访问

--- ps-server/src/main/java/com/syswin/temail/ps/server/AutoDecryptPsServer.java
+++ ps-server/src/main/java/com/syswin/temail/ps/server/AutoDecryptPsServer.java
@@ -5,8 +5,9 @@ import com.syswin.temail.ps.server.service.RequestService;
 import com.syswin.temail.ps.server.service.SessionService;
 
 /**
- * @author 姚华成
- * @date 2018-10-19
+ * <H1>AutoDecryptPsServer服务器对象</H1>
+ * 本类与PsServer的区别主要是：本类会自动对加密的数据进行解密。
+ * 因此需要配置解密所需要的KMS相关的信息：地址：vaultRegistryUrl，租户ID：tenantId
  */
 public class AutoDecryptPsServer extends PsServer {
 
