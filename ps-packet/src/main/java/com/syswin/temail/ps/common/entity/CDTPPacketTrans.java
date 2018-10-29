c4dab6ec3db48ece5c4f6e5122c29872a675d0d4
姚华成 <yaohuacheng@syswin.com>
2018-10-29 16:55:15 +0800

    整合dispatcher里的PacketUtil功能

--- ps-packet/src/main/java/com/syswin/temail/ps/common/entity/CDTPPacketTrans.java
+++ ps-packet/src/main/java/com/syswin/temail/ps/common/entity/CDTPPacketTrans.java
@@ -1,7 +1,8 @@
 package com.syswin.temail.ps.common.entity;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
+import static com.syswin.temail.ps.common.utils.PacketUtil.decodeData;
+import static com.syswin.temail.ps.common.utils.PacketUtil.encodeData;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -27,33 +28,9 @@ public class CDTPPacketTrans {
         encodeData(packet));
   }
 
-  private static byte[] decodeData(CDTPPacketTrans packet) {
-    String data = packet.getData();
-    if (data == null) {
-      return new byte[0];
-    }
-    if (isSendSingleMsg(packet.getCommandSpace(), packet.getCommand())) {
-      return Base64.getUrlDecoder().decode(data);
-    } else {
-      return data.getBytes(StandardCharsets.UTF_8);
-    }
-  }
-
-  private static String encodeData(CDTPPacket packet) {
-    if (isSendSingleMsg(packet.getCommandSpace(), packet.getCommand())) {
-      return Base64.getUrlEncoder().encodeToString(packet.getData());
-    } else {
-      return new String(packet.getData(), StandardCharsets.UTF_8);
-    }
-  }
-
-  private static boolean isSendSingleMsg(short commandSpace, short command) {
-    return commandSpace == CommandSpaceType.SINGLE_MESSAGE_CODE && command == 1;
-  }
-
   public CDTPPacket toCDTPPacket() {
     return new CDTPPacket(commandSpace, command, version, header.clone(),
-        decodeData(this));
+        decodeData(this, false));
   }
 
 }
