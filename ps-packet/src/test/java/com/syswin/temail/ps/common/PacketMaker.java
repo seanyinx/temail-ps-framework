f63ffcf06138cb765b8dcec5fd5e81902fc67000
殷湘 <yinxiang@syswin.com>
2018-11-13 19:16:37 +0800

    minor refactoring

--- ps-packet/src/test/java/com/syswin/temail/ps/common/PacketMaker.java
+++ ps-packet/src/test/java/com/syswin/temail/ps/common/PacketMaker.java
@@ -1,10 +1,15 @@
 package com.syswin.temail.ps.common;
 
+import static com.syswin.temail.ps.common.Constants.CDTP_VERSION;
+import static com.syswin.temail.ps.common.entity.CommandSpaceType.CHANNEL_CODE;
 import static com.syswin.temail.ps.common.entity.CommandSpaceType.SINGLE_MESSAGE_CODE;
 
 import com.google.gson.Gson;
 import com.syswin.temail.ps.common.entity.CDTPHeader;
 import com.syswin.temail.ps.common.entity.CDTPPacket;
+import com.syswin.temail.ps.common.entity.CDTPProtoBuf.CDTPLogin;
+import com.syswin.temail.ps.common.entity.CDTPProtoBuf.CDTPLogin.Builder;
+import com.syswin.temail.ps.common.entity.CommandType;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -17,7 +22,7 @@ public class PacketMaker {
 
   private static Gson gson = new Gson();
 
-  static CDTPPacket sendSingleCharPacket(String sender, String receiver, String content) {
+  public static CDTPPacket sendSingleCharPacket(String sender, String receiver, String content) {
     CDTPPacket packet = new CDTPPacket();
     packet.setCommandSpace(SINGLE_MESSAGE_CODE);
     packet.setCommand((short) 1);
@@ -41,6 +46,40 @@ public class PacketMaker {
 
     packet.setHeader(header);
     packet.setData(content.getBytes());
+    return packet;
+  }
+
+  public static CDTPPacket loginPacket(String sender, String deviceId) {
+    CDTPPacket packet = new CDTPPacket();
+    CDTPHeader header = new CDTPHeader();
+    header.setDeviceId(deviceId);
+    header.setSignatureAlgorithm(1);
+    header.setTimestamp(1535713173935L);
+    header.setDataEncryptionMethod(0);
+    header.setPacketId("PacketId12345");
+    header.setSender(sender);
+    header.setSenderPK("SenderPK");
+//    header.setReceiver("sean@t.email");
+//    header.setReceiverPK("ReceiverPK");
+
+    packet.setCommandSpace(CHANNEL_CODE);
+    packet.setCommand(CommandType.LOGIN.getCode());
+    packet.setVersion(CDTP_VERSION);
+    packet.setHeader(header);
+
+    Builder builder = CDTPLogin.newBuilder();
+
+//    builder.setdevId("设备ID");
+    builder.setPushToken("推送token");
+    builder.setPlatform("ios/android/pc");
+    builder.setOsVer("11.4");
+    builder.setAppVer("1.0.0");
+    builder.setLang("en、ch-zn...");
+    builder.setTemail("请求发起方的temail地址");
+    builder.setChl("渠道号");
+    CDTPLogin cdtpLogin = builder.build();
+
+    packet.setData(cdtpLogin.toByteArray());
     return packet;
   }
 
